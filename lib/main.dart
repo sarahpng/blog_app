7b07f48d7881772dfe696486da5e90ee5a0d5ab7
sarahpng <sarahwaseem21@gmail.com>
2025-11-12 21:34:52 +0500

    auth repository

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
 import 'package:blog_app/features/auth/presentation/pages/signup_page.dart';
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize(
+    url: String.fromEnvironment('SUPABASE_URL'),
+    anonKey: String.fromEnvironment('SUPABASE_KEY'),
+  );
   runApp(const MyApp());
 }
 
